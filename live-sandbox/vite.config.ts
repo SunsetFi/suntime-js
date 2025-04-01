1f33451da7bd19771843c5d3af156fc180046426
SunsetFi <sunsetfidev@gmail.com>
2025-04-01 09:33:51 -0700

    Fix base url

--- live-sandbox/vite.config.ts
+++ live-sandbox/vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
 export default defineConfig({
+  base: "/static-js/",
   plugins: [
     react(),
     checker({ typescript: { root: ".", tsconfigPath: "tsconfig.json" } }),
   ],
   define: {
-    "process.env": JSON.stringify({})
-  }
+    "process.env": JSON.stringify({}),
+  },
 });
