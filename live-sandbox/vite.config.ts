60fdbff81581abbed56a99e4019a0a5b262129d0
SunsetFi <sunsetfidev@gmail.com>
2025-04-09 12:26:25 -0700

    Fix sandbox path

--- live-sandbox/vite.config.ts
+++ live-sandbox/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
 export default defineConfig({
-  base: "/static-js/",
+  base: "/suntime-js/",
   plugins: [
     react(),
     checker({ typescript: { root: ".", tsconfigPath: "tsconfig.json" } }),
