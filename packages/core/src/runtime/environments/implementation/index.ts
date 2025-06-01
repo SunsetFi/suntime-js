3f08b23b2733624f093053e51576a4adec16a89e
SunsetFi <sunsetfidev@gmail.com>
2025-05-31 23:40:57 -0700

    Get rid of unused index file

--- packages/core/src/runtime/environments/implementation/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as StaticJsDeclarativeEnvironmentRecord } from "./StaticJsDeclarativeEnvironmentRecord.js";
-export { default as StaticJsFunctionEnvironmentRecord } from "./StaticJsFunctionEnvironmentRecord.js";
-export { default as StaticJsGlobalEnvironmentRecord } from "./StaticJsGlobalEnvironmentRecord.js";
-export { default as StaticJsLexicalEnvironment } from "./StaticJsLexicalEnvironment.js";
-export { default as StaticJsObjectEnvironmentRecord } from "./StaticJsObjectEnvironmentRecord.js";
