d2687fb52223289807e087f295b4dafb6d978880
SunsetFi <sunsetfidev@gmail.com>
2025-06-01 11:15:12 -0700

    Format staged

--- packages/core/lint-staged.config.js
+++ packages/core/lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   "*.ts": [
-    "pnpm run --filter @suntime-js/core format",
+    "pnpm run --filter @suntime-js/core format:staged",
     "pnpm run --filter @suntime-js/core lint",
     () => "pnpm run --filter @suntime-js/core check",
   ],
