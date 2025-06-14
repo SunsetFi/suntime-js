ee1b9efc529f935a007735b3c4ef263bea5f4396
SunsetFi <sunsetfidev@gmail.com>
2025-06-14 12:47:24 -0700

    Fix vitest

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vitest/config";
+import { readFileSync } from "fs";
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        esbuild: {
+          tsconfigRaw: readFileSync("./tsconfig.tests.json", "utf-8"),
+        },
+        test: {
+          name: "e2e",
+          include: ["./tests/e2e/**/*.ts"],
+          exclude: ["./tests/e2e/**/utils/*.ts"],
+        },
+      },
+      // {
+      //   esbuild: {
+      //     tsconfigRaw: readFileSync("./tsconfig.tests.json", "utf-8"),
+      //   },
+      //   test: {
+      //     name: "test262",
+      //     include: ["./tests/test262/test262.ts"],
+      //   },
+      // },
+    ],
+  },
+});
