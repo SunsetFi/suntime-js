3c4faa74b15f09a35364b1ab7e76ff7cdce88220
SunsetFi <sunsetfidev@gmail.com>
2025-07-01 17:29:29 -0700

    Notes on new generator design

--- packages/core/src/evaluator/evaluator-runtime.ts
+++ packages/core/src/evaluator/evaluator-runtime.ts
@@ -9,6 +9,11 @@ export function* evaluateCommands<TReturn>(
   generator: EvaluationGenerator<TReturn>,
   { onBeforeNodeEntry }: EvaluateCommandsOptions = {},
 ): Generator<void, TReturn, void> {
+  // At one point in time, all evaluators yielded 'evaluation requests', which we handled here in a stack.
+  // However, we need to allow evaluators to suspend execution on their own, for async and generator functions.
+  // Rather than creating a convoluted process where this function has to deal with it, I just changed
+  // the evaluate-node yield to be purely advisory, and not take any return value.
+
   while (true) {
     // We accept that calling the generator immediately without waiting for a yield will
     // start executing code, but the proper use should be that we start with a
@@ -19,11 +24,14 @@ export function* evaluateCommands<TReturn>(
     const { value, done } = generator.next(null);
 
     if (!done && value.kind === "evalute-node" && onBeforeNodeEntry) {
+      // We get this notification yielded before any node is actually evaluated.
       onBeforeNodeEntry(value.node);
     }
 
     // Wait for our runner to give us permission to continue.
     yield;
+
+    // If this is waiting on a node, continuing will evaluate that node.
 
     if (done) {
       return value;
