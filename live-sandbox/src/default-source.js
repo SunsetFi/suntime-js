a161e4ee317871707458a83ae5a487e931f54b14
SunsetFi <sunsetfidev@gmail.com>
2026-04-06 15:55:38 -0700

    More details in default sandbox code

--- live-sandbox/src/default-source.js
+++ live-sandbox/src/default-source.js
@@ -1,5 +1,25 @@
-// Hit F5 or use the debugger pane to run!
-// Breakpoints are supported!
+/**
+ * This is a demonstration of @suntime-js!
+ * https://github.com/SunsetFi/suntime-js
+ *
+ * This project provides a sandboxed javascript interpreter supporting modern features,
+ * that can run directly in the browser.
+ *
+ * The runtime environment is isolated, providing its own primitives and implementations of JavaScript constructs.
+ * Code ran in the interpreter cannot access the host's environment beyond what is explicitly provided.
+ *
+ * The interpreter can also run with time-slicing, ensuring infinite loops do not deadlock the browser.
+ * Try running an infinite loop, and see how the interpreter handles it!
+ */
+
+// Hit F5 or use the debugger pane to run.
+// Breakpoints and stack traces are supported, but variable and scope inspection is a work in progress.
+
+/*
+while(true) {
+  console.log("Look Around You")
+}
+*/
 
 function nthPrime(n) {
   const primes = [];
@@ -19,4 +39,4 @@ function findPrime(n) {
   console.log(`The ${n}th prime is ${prime}`);
 }
 
-findPrime(200);
+findPrime(300);
