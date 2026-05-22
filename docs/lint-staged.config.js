22036d1ecac279dc18805b610689ce63c322199c
SunsetFi <sunsetfidev@gmail.com>
2026-05-22 15:00:37 -0700

    Fix lint-staged for docs.  Fix deps

--- docs/lint-staged.config.js
+++ docs/lint-staged.config.js
@@ -1,12 +1,8 @@
-import { lintStagedWorkspaceRunner } from "../../workspace-utils.mjs";
+import { lintStagedWorkspaceRunner } from "../workspace-utils.mjs";
 
 const run = lintStagedWorkspaceRunner("@suntime-js/docs");
 
-const tsLint = [
-  run("format"),
-  run("lint"),
-  () => "pnpm run --filter @suntime-js/docs check",
-];
+const tsLint = [run("format"), run("lint"), () => "pnpm run --filter @suntime-js/docs check"];
 export default {
   "**/*.ts": tsLint,
   "!(src/static/playground-embed/**)": run("format"),
